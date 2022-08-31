a425f02aca42d9e277c3e21cbd8bb415d2f7a48a
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-08-31 14:25:03 +0500

    add sprites and basic configuration

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-import { grid } from './drawing.js' ;
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
 
-const canvas = document.getElementsByTagName('canvas')[0];
-const context = canvas.geContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+const spriteSheetWidth = 6876;
+const spriteSheetHeight = 5230;
+const rows = 12;
+const columns = 10;
+const spriteWidth = spriteSheetWidth / rows;
+const spriteHeight = spriteSheetHeight / columns;
 
+const playeImage = new Image();
+playeImage.src = "./images/shadow_dog.png";
 
-grid(canvas, context);
+const animate = (_) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // ctx.fillRect(100, 50, 100, 100);
+  ctx.drawImage(
+    playeImage,
+    spriteWidth * 0,
+    spriteHeight * 0,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    ctx.canvas.width,
+    ctx.canvas.height
+  );
+  requestAnimationFrame(animate);
+};
+
+animate();
